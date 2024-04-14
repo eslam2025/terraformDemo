78738bb586f39be10dc02d194ec1d6a859937109
eslam <loom2141@gmail.com>
2024-04-14 22:29:28 +0200

    3.3

--- demo.tf
+++ demo.tf
@@ -10,9 +10,26 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "subnet1" {
 
-
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.10.0/24"
   availability_zone = "eu-west-3a"
+  tags = {
+    Name = subnet1
+  }
+
+}
+
+data "aws_vpc" "existing_vpc"{
+
+  default =  true
+}
+
+resource "aws_subnet" "subnet2"{
+  
+  vpc_id = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.48.0/20"
+  tags = {
+    Name = subnet2
+  }
 
 }
